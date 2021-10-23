5509c0a7493b128559b437a7ae5d2df60af1ed50
stephho <ho.lingsum@gmail.com>
2021-10-23 14:06:55 -0400

    new functions create_db_pg_template, get_page_props

--- robinfolio/notion.py
+++ robinfolio/notion.py
@@ -98,3 +98,64 @@ def get_db_pg_ids(db_id):
         pg_dict[pg_name] = pg['id']
     
     return pg_dict 
+
+
+def create_db_pg_template(db_id, pg_icon=None): 
+    """
+    Create a template for the json data of a new Notion page in a database
+    The template is formatted according to the database's schema (properties) 
+    so that it can be used to create a new page in that database
+
+    Args: 
+        db_id (str): Notion database ID 
+        pg_icon (str): Optional. URL of an image where it is hosted. The image is used as the icon of the page
+
+    Returns: 
+        pg_template (dict): Template to be used to create a new page in the database
+        Keys are parent, icon, properties. Values need to be filled in to create a page 
+    """
+
+    db_schema = get_db_schema(db_id, simple=True)
+
+    pg_template = {
+        'parent': {'database_id': db_id}, 
+        'icon': {'type': 'external', 'external':{'url':pg_icon}}, 
+        'properties': {}
+    }
+
+    if pg_icon == None: 
+        pg_template.pop('icon')
+
+    for prop_name, prop_type in db_schema.items(): 
+        if prop_type == 'date': 
+            pg_template['properties'][prop_name] = {prop_type:{'start': None, 'end': None}}
+        elif prop_type == 'select': 
+            pg_template['properties'][prop_name] = {prop_type:{'id': None}}
+        elif prop_type == 'relation': 
+            pg_template['properties'][prop_name] = {prop_type:[{'id': None}]}
+        elif prop_type == 'title': 
+            pg_template['properties'][prop_name] = {prop_type:[{'text': {'content': None}}]}
+        else: # number and rich_text properties do not need special format 
+            pg_template['properties'][prop_name] = {prop_type:None}
+
+    return pg_template 
+    
+
+def get_page_props(pg_id, prop=None): 
+    """
+    Get the properties of a Notion page 
+
+    Args: 
+        pg_id (str): Notion page ID. Example: '522fccf3-f806-44a7-9560-1d094bebbe33'. Must have dash notation 
+        prop (str): Optional. Name of the property of the page to filter for
+
+    Returns: 
+        (dict): Keys are the property names. Values are the property types and the current value of the property 
+    """
+    pg_url = page_base_url + '/' + pg_id 
+    response = requests.get(pg_url, headers=notion_header)
+    pg_props = json.loads(response.text)['properties']
+    if prop == None: 
+        return pg_props 
+    else: 
+        return pg_props[prop]
