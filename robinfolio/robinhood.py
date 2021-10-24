54b58178fb1ccd7becd54309bdbd170c329c6fbc
stephho <ho.lingsum@gmail.com>
2021-10-24 19:59:56 -0400

    update get_instrument_id to also return stock name

--- robinfolio/robinhood.py
+++ robinfolio/robinhood.py
@@ -4,14 +4,10 @@ import requests
 from dotenv import load_dotenv
 
 
-# robinhood api credentials 
+# SET UP ROBINHOOD API 
+
 load_dotenv()
 rh_api_token = os.environ.get('RH_TOKEN')
-
-
-# robinhood api urls
-rh_orders_url = 'https://api.robinhood.com/orders/'
-rh_stocks_url = 'https://api.robinhood.com/instruments/'
 
 rh_header = {
     'authority':'api.robinhood.com', 
@@ -23,16 +19,26 @@ rh_header = {
     'Content-Type':'application/json'
 }
 
+# robinhood api urls
+rh_orders_url = 'https://api.robinhood.com/orders/'
+rh_stocks_url = 'https://api.robinhood.com/instruments/'
+
 
 def get_stock_name(instrument_id): 
     """
-    Get a stock's ticker symbol and full name, given Robinhood's instrument ID for that stock 
+    Get a stock's ticker symbol and name, given Robinhood's instrument ID for that stock 
 
     Args:  
-        instrument_id (str): Robinhood instrument ID, unique for each stock 
+        instrument_id (str): Robinhood unique alphaumneric ID for a stock 
     
     Returns: 
-        (tuple): Stock ticker symbol, stock name. Example: 'AAPL' is the stock ticker symbol, 'Apple' is the stock name
+        (tuple): 
+            stock_ticker (str): Stock ticker symbol
+            stock_name (str):: Stock name, i.e. company name
+    
+    Example: 
+        For Apple: 'AAPL' is the stock ticker symbol, 'Apple' is the stock name,
+        and the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
     """
     instrument_url = rh_stocks_url + instrument_id + '/'
     response = requests.get(instrument_url) # does not require authentication 
@@ -44,24 +50,32 @@ def get_stock_name(instrument_id):
 
 def get_instrument_id(stock_ticker): 
     """
-    Get Robinhood's instrument ID for a stock, given its ticker symbol 
+    Get Robinhood's instrument ID and name for a stock, given its ticker symbol 
 
     Args:  
-        stock_ticker (str): Stock ticker symbol. Example: 'AAPL' is the stock ticker symbol for Apple. Not case sensitive 
+        stock_ticker (str): Stock ticker symbol. Not case sensitive 
     
     Returns: 
-        instrument_id (str): Robinhood's unique alphanumeric ID for a stock. Example: The instrument ID for AAPL is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
+        (tuple): 
+            instrument_id (str): Robinhood's unique alphanumeric ID for a stock 
+            stock_name (str): Stock name, i.e. company name 
+    
+    Example: 
+        For Apple: 'AAPL' is the stock ticker symbol, 'Apple' is the stock name,
+        and the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
     """
     stock_ticker = stock_ticker.upper() 
     ticker_url = rh_stocks_url + '?symbol=' + stock_ticker
     response = requests.get(ticker_url) # does not require authentication 
-    response = json.loads(response.text)['results']
+    results = json.loads(response.text)['results']
     if results: # results is a list
         instrument_id = results[0]['id'] 
+        stock_name = results[0]['simple_name']
     else: # results list is empty 
         print('Instrument ID for stock ticker symbol {} not found'.format(stock_ticker))
         instrument_id = None
-    return instrument_id
+        stock_name = None
+    return (instrument_id, stock_name) 
 
     
 def get_order_history(instrument_id=None, stock_ticker=None): 
@@ -69,17 +83,21 @@ def get_order_history(instrument_id=None, stock_ticker=None):
     Get all filled (i.e. completed) buy and sell orders in a Robinhood account's history 
 
     Args:  
-        instrument_id (str): Optional filter for filled orders for a particular stock, by its Robinhood instrument ID. Example: Apple is '450dfc6d-5510-4d40-abfb-f633b7d9be3e' 
-        stock_ticker (str): Optional filter for filled orders for a particular stock ticker symbol. Example: 'AAPL' is the stock ticker symbol for Apple. Not case sensitive 
-        If both instrument_id and stock_ticker are passed, instrument_id is used 
-        If neither instrument_id nor stock_ticker is passed, filled orders for all stocks will be returned 
+        instrument_id (str): Optional filter for filled orders for a particular stock, by its Robinhood instrument ID
+            Example: Apple is '450dfc6d-5510-4d40-abfb-f633b7d9be3e' 
+        stock_ticker (str): Optional filter for filled orders for a particular stock ticker symbol
+            Example: 'AAPL' is the stock ticker symbol for Apple. Not case sensitive 
+            If both instrument_id and stock_ticker are passed, instrument_id is used 
+            If neither instrument_id nor stock_ticker is passed, filled orders for all stocks will be returned 
 
     Returns: 
         all_orders (list): A list of dictionaries, where each dictionary is an order and its associated information 
     """
 
     if instrument_id == None and stock_ticker != None: 
-        instr_id = get_instrument_id(stock_ticker=stock_ticker) 
+        instr_id, stock_name = get_instrument_id(stock_ticker=stock_ticker) 
+        if instr_id == None: 
+            return None
     else: 
         instr_id = instrument_id 
 
