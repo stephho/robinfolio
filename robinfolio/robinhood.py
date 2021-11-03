daa4e3661a086835d38befdbaf8a5a0f00c8d874
stephho <ho.lingsum@gmail.com>
2021-11-02 23:09:37 -0400

    create new class for robinhood api

--- robinfolio/robinhood.py
+++ robinfolio/robinhood.py
@@ -4,129 +4,143 @@ import requests
 from dotenv import load_dotenv
 
 
-# SET UP ROBINHOOD API 
 
-load_dotenv()
-rh_api_token = os.environ.get('RH_TOKEN')
-
-rh_header = {
-    'authority':'api.robinhood.com', 
-    'authorization':'Bearer ' + rh_api_token, 
-    'accept':'*/*',
-    'referrer':'https://robinhood.com/',
-    'accept-language':'en-US,en;q=0.9',
-    'x-robinhood-api-version':'1.431.4',
-    'Content-Type':'application/json'
-}
-
-# robinhood api urls
-rh_orders_url = 'https://api.robinhood.com/orders/'
-rh_stocks_url = 'https://api.robinhood.com/instruments/'
-
-
-def get_stock_name(instrument_id): 
+class robinhood: 
     """
-    Get a stock's ticker symbol and name, given Robinhood's instrument ID for that stock 
+    Class for interacting with Robinhood API 
 
-    Args:  
-        instrument_id (str): Robinhood unique alphaumneric ID for a stock 
-    
-    Returns: 
-        (tuple): 
-            stock_ticker (str): Stock ticker symbol
-            stock_name (str):: Stock name, i.e. company name
-    
-    Example: 
-        For Apple: 'AAPL' is the stock ticker symbol, 'Apple' is the stock name,
-        and the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
-    """
-    instrument_url = rh_stocks_url + instrument_id + '/'
-    response = requests.get(instrument_url) # does not require authentication 
-    response = json.loads(response.text)
-    stock_ticker = response['symbol']
-    stock_name = response['simple_name'] 
-    return (stock_ticker, stock_name)
-
-
-def get_instrument_id(stock_ticker): 
-    """
-    Get Robinhood's instrument ID and name for a stock, given its ticker symbol 
-
-    Args:  
-        stock_ticker (str): Stock ticker symbol. Not case sensitive 
-    
-    Returns: 
-        (tuple): 
-            instrument_id (str): Robinhood's unique alphanumeric ID for a stock 
-            stock_name (str): Stock name, i.e. company name 
-    
-    Example: 
-        For Apple: 'AAPL' is the stock ticker symbol, 'Apple' is the stock name,
-        and the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
-    """
-    stock_ticker = stock_ticker.upper() 
-    ticker_url = rh_stocks_url + '?symbol=' + stock_ticker
-    response = requests.get(ticker_url) # does not require authentication 
-    results = json.loads(response.text)['results']
-    if results: # results is a list
-        instrument_id = results[0]['id'] 
-        stock_name = results[0]['simple_name']
-    else: # results list is empty 
-        print('Instrument ID for stock ticker symbol {} not found'.format(stock_ticker))
-        instrument_id = None
-        stock_name = None
-    return (instrument_id, stock_name) 
-
-    
-def get_order_history(instrument_id=None, stock_ticker=None): 
-    """
-    Get all filled (i.e. completed) buy and sell orders in a Robinhood account's history 
-
-    Args:  
-        instrument_id (str): Optional filter for filled orders for a particular stock, by its Robinhood instrument ID
-            Example: Apple is '450dfc6d-5510-4d40-abfb-f633b7d9be3e' 
-        stock_ticker (str): Optional filter for filled orders for a particular stock ticker symbol
-            Example: 'AAPL' is the stock ticker symbol for Apple. Not case sensitive 
-            If both instrument_id and stock_ticker are passed, instrument_id is used 
-            If neither instrument_id nor stock_ticker is passed, filled orders for all stocks will be returned 
-
-    Returns: 
-        all_orders (list): A list of dictionaries, where each dictionary is an order and its associated information 
+    Attributes: 
+        api_token (str): Robinhood API token
+        api_header (dict): HTTP headers required by Robinhood API 
+        api_orders_url (str): Robinhood's API URL for interacting with order objects
+        api_stocks_url (str): Robinhood's API URL for interacting with stock objects, aka instruments
     """
 
-    if instrument_id == None and stock_ticker != None: 
-        instr_id, stock_name = get_instrument_id(stock_ticker=stock_ticker) 
-        if instr_id == None: 
-            return None
-    else: 
-        instr_id = instrument_id 
+    def __init__(self, api_token): 
+        self.api_token = api_token 
+        self.api_header = {
+            'authority':'api.robinhood.com', 
+            'authorization':'Bearer ' + self.api_token, 
+            'accept':'*/*',
+            'referrer':'https://robinhood.com/',
+            'accept-language':'en-US,en;q=0.9',
+            'x-robinhood-api-version':'1.431.4',
+            'Content-Type':'application/json'
+        }
+        self.api_orders_url = 'https://api.robinhood.com/orders/'
+        self.api_stocks_url = 'https://api.robinhood.com/instruments/'
 
-    all_orders = []
-    response = requests.get(rh_orders_url, headers=rh_header)
-    results = json.loads(response.text)['results'] 
-    next_page = json.loads(response.text)['next']
 
-    for r in results: 
-        # filter out cancelled or unfilled orders 
-        if r['side'] in ['buy', 'sell'] and r['state'] == 'filled': 
-            if instr_id == None: 
-                all_orders.append(r) 
-            else: 
-                if r['instrument_id'] == instr_id: 
-                    all_orders.append(r)
-    
-    while next_page: 
-        next_page = next_page.replace('http://loadbalancer-brokeback.nginx.service.robinhood', 'https://api.robinhood.com')
-        response = requests.get(next_page, headers=rh_header)
+    def get_stock_name(self, instrument_id): 
+        """
+        Get a stock's ticker symbol and name, given Robinhood's instrument ID 
+        for that stock 
+        
+        Args:  
+            instrument_id (str): Robinhood unique alphaumneric ID for a stock 
+        
+        Returns: 
+            (tuple): 
+                stock_ticker (str): Stock ticker symbol
+                stock_name (str): Stock name, i.e. company name
+        
+        Example: 
+            'Apple' is the stock name, 'AAPL' is the stock ticker symbol, and 
+            the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
+        """
+        instrument_url = self.api_stocks_url + instrument_id + '/'
+        response = requests.get(instrument_url) # does not require authentication 
+        response = json.loads(response.text)
+        stock_ticker = response['symbol']
+        stock_name = response['simple_name'] 
+        return (stock_ticker, stock_name)
+
+
+    def get_instrument_id(self, stock_ticker): 
+        """
+        Get Robinhood's instrument ID and name for a stock, given its ticker symbol 
+
+        Args:  
+            stock_ticker (str): Stock ticker symbol. Not case sensitive 
+        
+        Returns: 
+            (tuple): 
+                instrument_id (str): Robinhood's unique alphanumeric ID for a stock 
+                stock_name (str): Stock name, i.e. company name 
+        
+        Example: 
+            'Apple' is the stock name, 'AAPL' is the stock ticker symbol, and 
+            the instrument ID is '450dfc6d-5510-4d40-abfb-f633b7d9be3e'
+        """
+        stock_ticker = stock_ticker.upper() 
+        ticker_url = self.api_stocks_url + '?symbol=' + stock_ticker
+        response = requests.get(ticker_url) # does not require authentication 
         results = json.loads(response.text)['results']
+        if results: # results is a list
+            instrument_id = results[0]['id'] 
+            stock_name = results[0]['simple_name']
+        else: # results list is empty 
+            print('Instrument ID for stock ticker symbol {} not found'.format(stock_ticker))
+            instrument_id = None
+            stock_name = None
+        return (instrument_id, stock_name) 
+
+    
+    def get_order_history(self, instrument_id=None, stock_ticker=None): 
+        """
+        Get all filled (i.e. completed) buy and sell orders in a Robinhood 
+        account's history
+
+        Args:  
+            instrument_id (str): Optional filter for filled orders for a 
+                particular stock, by its Robinhood instrument ID
+                Example: Apple is '450dfc6d-5510-4d40-abfb-f633b7d9be3e' 
+            stock_ticker (str): Optional filter for filled orders for a 
+                particular stock ticker symbol. Not case sensitive 
+                Example: 'AAPL' is the stock ticker symbol for Apple
+                
+            If both instrument_id and stock_ticker are passed, instrument_id is 
+            used. If neither instrument_id nor stock_ticker is passed, filled 
+            orders for all stocks will be returned 
+
+        Returns: 
+            all_orders (list): A list of dictionaries, where each dictionary is 
+                an order and its associated information 
+        """
+
+        if instrument_id == None and stock_ticker != None: 
+            instr_id, stock_name = self.get_instrument_id(stock_ticker=stock_ticker) 
+            if instr_id == None: 
+                return None
+        else: 
+            instr_id = instrument_id 
+
+        all_orders = []
+        response = requests.get(self.api_orders_url, headers=self.api_header)
+        results = json.loads(response.text)['results'] 
         next_page = json.loads(response.text)['next']
 
         for r in results: 
-            if r['side'] in ['buy', 'sell'] and r['state'] == 'filled':
+            # filter out cancelled or unfilled orders 
+            if r['side'] in ['buy', 'sell'] and r['state'] == 'filled': 
                 if instr_id == None: 
                     all_orders.append(r) 
                 else: 
                     if r['instrument_id'] == instr_id: 
                         all_orders.append(r)
-    
-    return all_orders 
+        
+        while next_page: 
+            next_page = next_page.replace('http://loadbalancer-brokeback.nginx.service.robinhood', 'https://api.robinhood.com')
+            response = requests.get(next_page, headers=self.api_header)
+            results = json.loads(response.text)['results']
+            next_page = json.loads(response.text)['next']
+
+            for r in results: 
+                if r['side'] in ['buy', 'sell'] and r['state'] == 'filled':
+                    if instr_id == None: 
+                        all_orders.append(r) 
+                    else: 
+                        if r['instrument_id'] == instr_id: 
+                            all_orders.append(r)
+        
+        return all_orders 
